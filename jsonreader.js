b2f949e042f6f54f95b675258a2ce10722445320
almoxxx <120190420+almoxxx@users.noreply.github.com>
2024-04-13 17:02:57 +0200

    enabled links

--- jsonreader.js
+++ jsonreader.js
@@ -6,14 +6,14 @@ fetch("Events.json")
     let htmlElements = data.events.map((event) => {
       return `<div class="row mb-2">
           <div class="col">
-<div class="card border border-4 border-primary" style="width: 18rem;">
+<div class="card" style="width: 18rem;">
   <div class="card-body bg-light rounded">
     <h5 class="card-title">${event.title}</h5>
     <h6 class="card-subtitle mb-2"><strong>Wann: </strong> ${event.date}</h6>
      <h6 class="card-subtitle mb-2"> <strong>Wo: </strong>${event.location}</h6>
     <h6 class="card-text"><strong>Kategorie: </strong>  ${event.type}</h6>
     <h6 class="card-text"><strong>Gratis: </strong>  ${event.isCost}</h6>
-    <a href="#" class="card-link">Event Link</a>
+    <a href=${event.link} target= "_blank" class="card-link">Event Link</a>
    
   </div>
 </div>
@@ -32,16 +32,16 @@ console.log(data);
 if (data) {
   data.reverse();
   let htmlElementsLocal = data.map((event) => {
-    return `<div class="row mb-2">
+    return `<div class="row">
         <div class="col">
-<div class="card border border-4 border-primary" style="width: 18rem;">
+<div class="card" style="width: 18rem;">
   <div class="card-body bg-light rounded">
     <h5 class="card-title">${event.title}</h5>
     <h6 class="card-subtitle mb-2 "><strong>Wann: </strong> ${event.date}</h6>
      <h6 class="card-subtitle mb-2 "><strong>Wo: </strong> ${event.location}</h6>
     <h6 class="card-text"><strong>Kategorie: </strong> ${event.type}</h6>
     <h6 class="card-text"> <strong>Free: </strong>${event.isCost}</h6>
-    <a href="#" class="card-link">Event Link</a>
+    <a href=${event.link} target= "_blank"  class="card-link">Event Link</a>
    
   </div>
 </div>
