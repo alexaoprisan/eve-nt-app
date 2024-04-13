f0f5f9d079e1981de9e62a63b210294fa30cc24d
Viktoria Dudina <viktoria.a.duidna@gmail.com>
2024-04-13 12:32:49 +0200

    json

---
+++ datafile.js
@@ -0,0 +1,14 @@
+const data = fs.readFileSync('data.json');
+
+const jsonData = JSON.parse(data);
+
+jsonData.event.push(
+    {
+        titel: ,
+        address: ,
+        dateFrom: ,
+        dateTo: ,
+        tag: ,
+        free: 
+  },
+);
