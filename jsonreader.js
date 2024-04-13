8b8888333b8d87898e6b8a60249941d7d97b8559
almoxxx <120190420+almoxxx@users.noreply.github.com>
2024-04-13 14:58:33 +0200

    localstorage parse funktion

--- jsonreader.js
+++ jsonreader.js
@@ -9,10 +9,10 @@ fetch("Events.json")
 <div class="card" style="width: 18rem;">
   <div class="card-body bg-success rounded">
     <h5 class="card-title">${event.title}</h5>
-    <h6 class="card-subtitle mb-2 text-body-secondary">Wann: ${event.dateFrom} bis ${event.dateTo}</h6>
+    <h6 class="card-subtitle mb-2 text-body-secondary">Wann: ${event.date}</h6>
      <h6 class="card-subtitle mb-2 text-body-secondary">Wo: ${event.location}</h6>
-    <p class="card-text">Kategorie: ${event.tag}</p>
-    <p class="card-text">Free: ${event.free}</p>
+    <p class="card-text">Kategorie: ${event.type}</p>
+    <p class="card-text">Free: ${event.isCost}</p>
     <a href="#" class="card-link">Event Link</a>
    
   </div>
@@ -26,3 +26,30 @@ fetch("Events.json")
   .catch((error) => {
     console.error("Error:", error);
   });
+
+let data = JSON.parse(localStorage.getItem("events"));
+console.log(data);
+if (data) {
+  let htmlElementsLocal = data.map((event) => {
+    return `<div class="row mb-2">
+        <div class="col">
+<div class="card" style="width: 18rem;">
+  <div class="card-body bg-success rounded">
+    <h5 class="card-title">${event.title}</h5>
+    <h6 class="card-subtitle mb-2 text-body-secondary">Wann: ${event.date}</h6>
+     <h6 class="card-subtitle mb-2 text-body-secondary">Wo: ${event.location}</h6>
+    <p class="card-text">Kategorie: ${event.type}</p>
+    <p class="card-text">Free: ${event.isCost}</p>
+    <a href="#" class="card-link">Event Link</a>
+   
+  </div>
+</div>
+        </div>
+        </div>`;
+  });
+
+  document.querySelector(".localstorageListe").innerHTML =
+    htmlElementsLocal.join("");
+} else {
+  console.error("No data in local storage");
+}
